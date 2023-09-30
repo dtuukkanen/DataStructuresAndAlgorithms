2d101af3b93595cafd13caf4f2fa001d0a04c680
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2023-09-30 21:57:38 +0300

    First exercise done

--- Assignment4/hashlinear.py
+++ Assignment4/hashlinear.py
@@ -3,41 +3,75 @@ class HashLinear:
         self.M = M
         self.T = [None] * M
 
-    # Hash retuns index of the table
     def hash(self, data):
+        # Hash is sum of ASCII values of characters in data
+        # Which is then modded by the size of the table
         sum = 0
         for i in range(len(data)):
             sum += ord(data[i])
         return sum % self.M
 
-    # Inserts data into the table
     def insert(self, data):
-        counter = 0
+        # Find index by hashing
+        index = self.hash(data)
 
-        i = self.hash(data)
-        # If the position is occupied, find the next empty position
-        while self.T[i] != None:
-            # If the table is full, return
-            if counter == self.M:
-                return
-            # If the data is already in the table, return
-            if self.T[i] == data:
-                return
-            # Else, find the next empty position
-            i = (i + 1) % self.M
-            counter += 1
-        self.T[i] = data
+        # Check if the index is empty or already has the data
+        if self.T[index] is None or self.T[index] == data:
+            self.T[index] = data
 
-    # Delete data from the table
+        # If not, find the next empty position
+        else:
+            i = 1
+            done = False
+            while not done:
+                # Probing
+                new_index = (index + i) % self.M
+
+                # Check and make sure that table is not full
+                if new_index == index:
+                    done = True
+
+                # Check if the index is empty or already has the data
+                if self.T[new_index] is None or self.T[new_index] == data:
+                    self.T[new_index] = data
+                    done = True
+
+                # If not, keep probing
+                else:
+                    i += 1
+
     def delete(self, data):
-        i = self.hash(data)
-        while self.T[i] != None:
-            if self.T[i] == data:
-                self.T[i] = None
-                return
-            i = (i + 1) % self.M
+        # Find index by hashing
+        index = self.hash(data)
+
+        # Check if the index has the data
+        if self.T[index] == data:
+            self.T[index] = None
+
+        # If not, find the next position that might have the data
+        else:
+            i = 1
+            done = False
+            while not done:
+                # Probing
+                new_index = (index + i) % self.M
+
+                # End if there isn't item to be deleted
+                if new_index == index:
+                    done = True
+
+                # Check if the index has the data
+                # If so, delete it
+                if self.T[new_index] == data:
+                    self.T[new_index] = None
+                    done = True
+
+                # If not, keep probing
+                else:
+                    i += 1
 
     def print(self):
+        # Print value if it is not None
         for i in range(self.M):
             if self.T[i] != None:
                 print(self.T[i], end=" ")
@@ -52,12 +86,12 @@ if __name__ == "__main__":
     table.insert("cheiromegaly")
     table.insert("premillennialise")
     table.insert("finebent")
-    table.print()
+    table.print()  # buttermilk shim cheiromegaly finebent resolvend premillennialise
     table.delete("buttermilk")
     table.delete("cores")
     table.delete("cheiromegaly")
     table.delete("iodations")
-    table.print()
+    table.print()  # shim finebent resolvend premillennialise
     table.insert("iodations")
     table.insert("tirrlie")
     table.insert("comous")
@@ -65,4 +99,4 @@ if __name__ == "__main__":
     table.insert("flabbergasts")
     table.insert("rename")
     table.insert("softhead")
-    table.print()
+    table.print()  # iodations discursiveness comous shim tirrlie finebent flabbergasts rename resolvend premillennialise
